56e111bc893fbde06b4deaa9e843cd2b8e529d96
Jordan Tranchina <Jordan12522@gmail.com>
2021-02-15 22:46:05 -0600

    Succesfully leaf-shook the document. This is the final commit!

--- movies.js
+++ movies.js
@@ -1,25 +1,10 @@
-// For this exercise, we'll be using the "now playing" API endpoint
 // https://api.themoviedb.org/3/movie/now_playing?api_key=426eb0f90521d4c76fbc67a9acd43da6&language=en-US
 
-// Note: image data returned by the API will only give you the filename;
-// prepend with `https://image.tmdb.org/t/p/w500/` to get the 
-// complete image URL
-
 window.addEventListener('DOMContentLoaded', async function (event) {
-  // Step 1: Construct a URL to get movies playing now from TMDB, fetch
-  // data and put the Array of movie Objects in a variable called
-  // movies. Write the contents of this array to the JavaScript
-  // console to ensure you've got good data
-  // ⬇️ ⬇️ ⬇️
+
   let db = firebase.firestore()
   let querySnapshot = await db.collection('watched').get()
   let movieList = querySnapshot.docs
-  for (let i = 0; i < movieList.length; i++) {
-    let movieData = movieList[i].data() // object / array level
-    let movieName = movieData.movie  // attribute level
-    // console.log(movieData);
-    // console.log(movieName);
-  }
 
   let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=426eb0f90521d4c76fbc67a9acd43da6&language=en-US`)
   let json = await response.json()
@@ -27,14 +12,11 @@ window.addEventListener('DOMContentLoaded', async function (event) {
 
   for (let i = 0; i < movies.length; i++) {
     let movie = movies[i]
-    //  TURN THIS ON BEFORE SUBMITTING
-    // console.log(movie);
+    console.log(movie);
     let movieID = movie.id
     let movieTitle = movie.original_title
     let moviePosterFileName = movie.poster_path
     let moviePoster = `https://image.tmdb.org/t/p/w500/${moviePosterFileName}`
-    // console.log(`${movieID} - ${movieTitle}`);
-    // console.log(moviePoster);
     let docRef = await db.collection('watched').doc(`${movieID}`).get()
     let item = docRef.data()
     console.log(item);
@@ -43,30 +25,10 @@ window.addEventListener('DOMContentLoaded', async function (event) {
     movieListener(movieID, movieTitle)
 
   }
-  // ⬆️ ⬆️ ⬆️ 
-  // End Step 1
 
-  // Step 2: 
-  // - Loop through the Array called movies and insert HTML
-  //   into the existing DOM element with the class name .movies
-  // - Include a "watched" button to click for each movie
-  // - Give each "movie" a unique class name based on its numeric
-  //   ID field.
-  // Some HTML that would look pretty good... replace with real values :)
-  // <div class="w-1/5 p-4 movie-abcdefg1234567">
-  //   <img src="https://image.tmdb.org/t/p/w500/moviePosterPath.jpg" class="w-full">
-  //   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
-  // </div>
-  // ⬇️ ⬇️ ⬇️
   async function printMovie(movieID, moviePoster, item) {
-    // console.log(moviePoster);
-
-    // if movie has been watched then ensure opacity is 20
-    // let docRef = await db.collection('watched').doc(`${movieID}`).get()
-    // console.log(docRef);
 
     if (item) {
-      console.log("this movie exists");
       document.querySelector(".movies").insertAdjacentHTML("beforeend",
         `
           <div class="w-1/5 p-4 movie-${movieID} opacity-20">
@@ -76,7 +38,6 @@ window.addEventListener('DOMContentLoaded', async function (event) {
         `
       )
     } else {
-      console.log("this movie does not exist");
       document.querySelector(".movies").insertAdjacentHTML("beforeend",
         `
           <div class="w-1/5 p-4 movie-${movieID}">
@@ -86,68 +47,15 @@ window.addEventListener('DOMContentLoaded', async function (event) {
         `
       )
     }
-    // else, movie should have full opacity
-
-    // document.querySelector(".movies").insertAdjacentHTML("beforeend",
-    //   `
-    //   <div class="w-1/5 p-4 movie-${movieID}">
-    //     <img src="${moviePoster}" class="w-full">
-    //     <a href="#" class="watched-button-${movieID} block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
-    //   </div>
-    //   `
-    // )
   }
-  // ⬆️ ⬆️ ⬆️ 
-  // End Step 2
 
-  // Step 3: 
-  // - Attach an event listener to each "watched button"
-  // - Be sure to prevent the default behavior of the button
-  // - When the "watched button" is clicked, changed the opacity
-  //   of the entire "movie" by using .classList.add('opacity-20')
-  // - When done, refresh the page... does the opacity stick?
-  // - Bonus challenge: add code to "un-watch" the movie by
-  //   using .classList.contains('opacity-20') to check if 
-  //   the movie is watched. Use .classList.remove('opacity-20')
-  //   to remove the class if the element already contains it.
-
-
-
-
-
-  // ⬇️ ⬇️ ⬇️
   function movieListener(movieID, movieName) {
     document.querySelector(`.watched-button-${movieID}`).addEventListener("click", async function (event) {
       event.preventDefault()
       document.querySelector(`.movie-${movieID}`).classList.add('opacity-20')
-      console.log(`Someone watched movie ${movieID}.`)
-
       await db.collection('watched').doc(`${movieID}`).set({
         movie: `${movieName}`
       })
-      // movieChecker(635302)
     })
   }
-  // ⬆️ ⬆️ ⬆️ 
-  // End Step 3
-
-
-  // Step 4: 
-
-  // - Properly configure Firebase and Firebase Cloud Firestore
-  // - Inside your "watched button" event listener, you wrote in
-  //   step 3, after successfully setting opacity, persist data
-  //   for movies watched to Firebase.
-  // - The data could be stored in a variety of ways, but the 
-  //   easiest approach would be to use the TMDB movie ID as the
-  //   document ID in a "watched" Firestore collection.
-  // - Hint: you can use .set({}) to create a document with
-  //   no data – in this case, the document doesn't need any data;
-  //   if a TMDB movie ID is in the "watched" collection, the 
-  //   movie has been watched, otherwise it hasn't.
-  // - Modify the code you wrote in Step 2 to conditionally
-  //   make the movie opaque if it's already watched in the 
-  //   database.
-  // - Hint: you can use if (document) with no comparison
-  //   operator to test for the existence of an object.
 })
